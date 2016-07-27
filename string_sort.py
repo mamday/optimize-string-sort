6542d5939604602ec2e517e1ea32a222382859c0
Melanie Day <mamday@gmail.com>
2016-07-27 15:56:17 -0700

    Bare bones layout of alphanumeric sortable string class

---
+++ string_sort.py
@@ -0,0 +1,20 @@
+''' Importing regular expressions package re to use re.sub() 
+    https://docs.python.org/2/library/re.html'''
+import re
+'''Importing Queue.Queue() from package Queue'''
+from Queue import Queue
+from string_tools import StringCleaner
+
+class AlphanumericSortableString(object):
+  def __init__(self):
+    self.num_heap = Queue()
+    self.str_heap = Queue()
+    self.type_heap = Queue()
+    self.cur_string = '' 
+  def __str__(self):
+    return self.cur_string
+  def make_heaps(self,in_string):
+
+  def string_sort(self,in_string):
+
+
